291085bae3eba60cda4ae862e152fcf92dd43b96
alok-pol <ravikantpal010@gmail.com>
2020-01-30 15:58:24 +0530

    final

--- src/main/java/com/alok/projectcars/controller/DriverController.java
+++ src/main/java/com/alok/projectcars/controller/DriverController.java
@@ -50,7 +50,6 @@ public class DriverController extends DriverDo {
                                                               @RequestParam(required = false, name = "engineType") String engineType) {
         List<Driver> list;
         //        System.out.println(manufacturer+" "+engineType);
-//
         try {
 
             if(manufacturer != null) {
